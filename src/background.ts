eaac0df6ffe278df29eb79c9018ad66e062e6edc
Edgar <starguitar307@gmail.com>
2021-01-07 22:14:17 +0200

    feat: add button to menu in every page

--- src/background.ts
+++ src/background.ts
@@ -8,11 +8,13 @@ polling();
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
   if (changeInfo.status === "complete") {
     chrome.tabs.get(tabId, (tab) => {
-      if(/https:\/\/github.com\/[a-zA-Z1-9\-]+(\/)?$/.test(tab.url)) {
+      const url = new URL(tab.url);
+
+      if (url.hostname === "github.com") {
         chrome.tabs.executeScript(tabId, {
           file: "js/github_messenger_payload.js",
         });
       }
-    })
+    });
   }
 });
--- src/github-messenger-payload.ts
+++ src/github-messenger-payload.ts
@@ -1,24 +1,29 @@
-console.log('GITHUB MESSENGER PAYLOAD IS INJECTED')
+console.log("GITHUB MESSENGER PAYLOAD IS INJECTED");
 
-const nav = document.querySelector('nav[aria-label=Global]')
+const nav = document.querySelector("nav[aria-label=Global]");
 
-const newAnchorElement = document.createElement('a')
+if (nav) {
+  if (!document.querySelector('a[data-messenger="button-in-menu"]')) {
+    const newAnchorElement = document.createElement("a");
 
-newAnchorElement.innerHTML = `
-<a
-  class="js-selected-navigation-item Header-link mt-md-n3 mb-md-n3 py-2 py-md-3 mr-0 mr-md-3 border-top border-md-top-0 border-white-fade-15"
-  data-ga-click="Header, click, Nav menu - item:explore"
-  data-selected-linkss="/explore /trending /trending/developers /integrations /integrations/feature/code /integrations/feature/collaborate /integrations/feature/ship showcases showcases_search showcases_landing /explore"
-  href="#"
-  style="position: relative"
->
-  Messenger
-  <div
-    style="position: absolute; background: red; width: 14pt; height: 14pt; text-align: center; font-size: 8pt; line-height: 14pt; border-radius: 50%; top: 6pt; right: -12pt;"
-  >
-    +9
-  </div>
-</a>
-`
+    newAnchorElement.innerHTML = `
+    <a
+      class="js-selected-navigation-item Header-link mt-md-n3 mb-md-n3 py-2 py-md-3 mr-0 mr-md-3 border-top border-md-top-0 border-white-fade-15"
+      data-ga-click="Header, click, Nav menu - item:explore"
+      data-selected-linkss="/explore /trending /trending/developers /integrations /integrations/feature/code /integrations/feature/collaborate /integrations/feature/ship showcases showcases_search showcases_landing /explore"
+      href="#"
+      style="position: relative"
+      data-messenger="button-in-menu"
+    >
+      Messenger
+      <div
+        style="position: absolute; background: red; width: 14pt; height: 14pt; text-align: center; font-size: 8pt; line-height: 14pt; border-radius: 50%; top: 6pt; right: -12pt;"
+      >
+        +9
+      </div>
+    </a>
+    `;
 
-nav.appendChild(newAnchorElement)
+    nav.appendChild(newAnchorElement);
+  }
+}
